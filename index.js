511e6b05ab2dc48048b1b36585aec3db256dd959
Gentry Riggen <gentryriggen@outlook.com>
2017-05-07 08:21:43 -0600

    Upgraded to react-native 0.44.0

--- index.js
+++ index.js
@@ -1,33 +1,36 @@
-import AnimatedView from './components/AnimatedView';
-import Avatar from './components/Avatar';
-import BarChart from './components/BarChart';
-import BottomDrawer from './components/BottomDrawer';
-import Button from './components/Button';
-import Checkbox from './components/Checkbox';
-import Constants from './components/lib/constants';
-import Icon from './components/Icon';
-import { focusTextInput } from './components/utils';
-import Gap from './components/Gap';
-import GradientView from './components/GradientView';
-import LineChart from './components/LineChart';
-import List from './components/List';
-import ListItem from './components/ListItem';
-import ListSwipeButton from './components/ListSwipeButton';
-import Modal from './components/Modal';
-import NavBar from './components/NavBar';
-import Page from './components/Page';
-import Picker from './components/Picker';
-import PieChart from './components/PieChart';
-import RadioButton from './components/RadioButton';
-import SearchBar from './components/SearchBar';
-import Spinner from './components/Spinner';
-import StyleSheet from './components/lib/StyleSheet';
-import Tab from './components/Tab';
-import TabBar from './components/TabBar';
-import Text from './components/Text';
-import TextInput from './components/TextInput';
-import theme, { getTheme } from './components/lib/theme';
-import TopTabs from './components/TopTabs';
+import {
+  AnimatedView,
+  Avatar,
+  BarChart,
+  BottomDrawer,
+  Button,
+  Checkbox,
+  Constants,
+  focusTextInput,
+  getTheme,
+  Gap,
+  GradientView,
+  Icon,
+  LineChart,
+  List,
+  ListItem,
+  ListSwipeButton,
+  Modal,
+  NavBar,
+  Page,
+  Picker,
+  PieChart,
+  RadioButton,
+  SearchBar,
+  Spinner,
+  StyleSheet,
+  Tab,
+  TabBar,
+  Text,
+  TextInput,
+  theme,
+  TopTabs,
+} from './components';
 
 module.exports = {
   AnimatedView,
